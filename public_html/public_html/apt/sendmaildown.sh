2983b847ec31b4983c2e686285046f072484fdf2
zharfanf <zharfanf@gmail.com>
2024-08-12 15:45:36 +0700

    update content

--- public_html/public_html/apt/sendmaildown.sh
+++
@@ -1,8 +0,0 @@
-sleep 2
-mailbody="SERVER bomblab is DOWN!!"
-echo "From: daniar.h.k@gmail.com" > /tmp/mailtest
-echo "To: daniar@uchicago.edu" >> /tmp/mailtest
-echo "Subject: [Apt Bot-Bomblab] DOWN DOWN $((1 + RANDOM % 1000))" >> /tmp/mailtest
-echo "" >> /tmp/mailtest
-echo $mailbody >> /tmp/mailtest
-cat /tmp/mailtest | /usr/sbin/sendmail -t	
